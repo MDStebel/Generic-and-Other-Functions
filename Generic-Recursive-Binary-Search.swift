4695b3ff29deb96e6a08914ecf56e3268fbb4a1b
Michael Stebel <michael@michaelstebelconsulting.com>
2018-05-02 18:21:06 -0400

    Changed parameter names
    Change parameter names to comply with Apple's API guidelines.

--- Generic-Recursive-Binary-Search.swift
+++ Generic-Recursive-Binary-Search.swift
@@ -1,24 +1,23 @@
 import Foundation
 
-// Recursive generic binary search
-func binarySearch<T: Comparable>(a: [T], key: T, range: Range<Int>) -> Int? {
+func binarySearch<T: Comparable>(in collection: [T], for key: T, range: Range<Int>) -> Int? {
     if range.lowerBound >= range.upperBound {
         // If we get here, then the search key is not present in the array.
         return nil
         
     } else {
-            // Calculate where to split the array.
+        // Calculate where to split the array.
         let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
         
-            // Is the search key in the left half?
-        if a[midIndex] > key {
-            return binarySearch(a: a, key: key, range: range.lowerBound ..< midIndex)
+        // Is the search key in the left half?
+        if collection[midIndex] > key {
+            return binarySearch(in: collection, for: key, range: range.lowerBound ..< midIndex)
             
-            // Is the search key in the right half?
-        } else if a[midIndex] < key {
-            return binarySearch(a: a, key: key, range: midIndex + 1 ..< range.upperBound)
+        // Is the search key in the right half?
+        } else if collection[midIndex] < key {
+            return binarySearch(in: collection, for: key, range: midIndex + 1 ..< range.upperBound)
             
-            // If we get here, then we've found the search key!
+        // If we get here, then we've found the search key!
         } else {
             return midIndex
         }
@@ -26,10 +25,12 @@ func binarySearch<T: Comparable>(a: [T], key: T, range: Range<Int>) -> Int? {
 }
 
 
-// Test examples
-let numbers = (1...1000000).map {$0}    // Initialize an array of integers
+/// Test it
+
+// Initialize an array of integers
+let numbers = (1...100000).map {$0}
 let averageInterations = Int(log2(Double(numbers.last!)))
 
-if let found = binarySearch(a: numbers, key: 16, range: 0..<numbers.count) {
-    print("Found")
+if let found = binarySearch(in: numbers, for: 500, range: 0..<numbers.count) {
+    print("Found at \(found)")
 }
