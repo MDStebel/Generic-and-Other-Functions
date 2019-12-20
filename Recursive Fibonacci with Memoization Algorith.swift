f2c31785c0fd21a2f3ece247e21b5f108e5c4c37
Michael Stebel <michael@michaelstebelconsulting.com>
2019-12-19 19:00:00 -0500

    Updated

--- Recursive Fibonacci with Memoization Algorith.swift
+++ Recursive Fibonacci with Memoization Algorith.swift
@@ -12,22 +12,21 @@ func fibonacci(ofOrder a: Int) -> Int64? {
     let max = 92
     guard (0...max ~= a) && a >= 0 else { return nil } // Using pattern matching operator to check range of a
     
-    var cachedFibs = [Int64](repeating: 0, count: max+1)
+    var cachedFibs = [Int64](repeating: -1, count: max+1)
     
-    func recursiveFib(ofOrder a: Int) -> Int64 {
+    func recursiveFib(ofOrder a1: Int) -> Int64 {
         var value: Int64 = 0
-        if cachedFibs[a] > 0 {
-            return cachedFibs[a]
+        if cachedFibs[a1] > 0 {
+            return cachedFibs[a1]
         }
-        if a <= 2 {
+        if a1 <= 2 {
             value = 1
         } else {
-            value = recursiveFib(ofOrder: a - 1) + recursiveFib(ofOrder: a - 2)
+            value = recursiveFib(ofOrder: a1 - 1) + recursiveFib(ofOrder: a1 - 2)
         }
-        cachedFibs[a] = value
+        cachedFibs[a1] = value
         return value
     }
-    
+
     return recursiveFib(ofOrder: a)
-    
 }
