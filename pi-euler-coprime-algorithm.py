51b6a2057925c8e92709a35730c4583ffe03b0e1
Michael Stebel <michael@michaelstebelconsulting.com>
2022-03-14 13:38:47 -0400

    Update pi-euler-coprime-algorithm.py

--- pi-euler-coprime-algorithm.py
+++ pi-euler-coprime-algorithm.py
@@ -2,23 +2,22 @@ import random
 import math
 from functools import cache
 
-
 @cache
+# Recursive greatest common divisor function
 def gcd(a: int, b: int) -> int:
     if b == 0:
         return a
     else:
         return gcd(b, a % b)
 
-
-# Create an array of random integers
-size = 50000000
+# Create an array of integers and fill it with n random integers
+n = 10000000
 randoms = [int]
-for i in range(size):
-    rnd_number = random.randint(1, size)
+for i in range(n):
+    rnd_number = random.randint(1, n)
     randoms.append(rnd_number)
 
-# Compute pi as the probability that any two integers are relatively prime (coprime)
+# The probability that two random numbers are co-prime is equal to 6/pi^2
 total_count: int = 0
 coprimes: int = 1
 index: int = 1
@@ -30,7 +29,7 @@ b2: int = 0
 for i in range(count):
     a2 = randoms[index]
     b2 = randoms[index + 1]
-    # Swap if b2 is large than a2
+    # Swap if b2 is larger than a2
     if b2 > a2:
         [a2, b2] = [b2, a2]
     if gcd(a2, b2) == 1:
@@ -38,5 +37,4 @@ for i in range(count):
     total_count += 1
     index += 2
     pi_so_far = math.sqrt(6.0 * total_count / coprimes)
-
-print(pi_so_far)
+    print(pi_so_far)
